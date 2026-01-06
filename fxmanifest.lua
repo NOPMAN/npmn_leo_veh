b7dd3947d4293c2c36a2f097779ca5b509c033c2
NOPMAN <104951540+NOPMAN@users.noreply.github.com>
2026-01-06 23:47:58 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,68 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'NOPMAN <@NOPMAN#1991>'
+description 'Nopman Corporation Law Enforcement vehicles pack (SA)'
+version '2.0.0'
+ 
+files {
+	'data/vehiclelayouts_bcat.meta',
+	'data/vehiclelayouts_anipp.meta',
+	'data/vehiclelayouts_lssd.meta',
+	'data/vehiclelayouts_lssd2.meta',
+	'data/vehiclelayouts_lssd3.meta',
+	'data/vehiclelayouts_jusmelspd.meta',
+	'data/vehiclelayouts_jmbearcat.meta',
+	'data/vehiclelayouts_buf4.meta',
+	'data/vehiclelayouts_rhpd.meta',
+	'data/vehiclelayouts_lvpd.meta',
+	'data/vehiclelayouts_swattruck.meta',
+	'data/vehiclelayouts_raiden.meta',
+	'data/handling.meta',
+	'data/vehicles.meta',
+	'data/carcols.meta',
+	'data/carcols_wheels_leo.meta',
+	'data/carvariations.meta',
+	'data/vehicleweapon_bcat.meta',
+
+	"audio/*.rel",
+	"audio/sfx/**/*.awc"
+}
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts_bcat.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts_anipp.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts_lssd.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts_lssd2.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts_lssd3.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts_jusmelspd.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts_buf4.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts_rhpd.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts_lvpd.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts_jmbearcat.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts_swattruck.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts_raiden.meta'
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+
+data_file 'CARCOLS_FILE' 'data/carcols_wheels_leo.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+data_file 'WEAPON_METADATA_FILE' 'data/vehicleweapon_bcat.meta'
+data_file 'AUDIO_GAMEDATA' 'audio/sapolicing_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/jmbearcat_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/sapolicing_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/jmsahp_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/baryscout_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/leoaleutian_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/dlc_delperropd_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/rhpd_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/dlcbarysahp_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/nachoscout_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/polraiden_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/lsiapd_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/nalamotrafpol_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/nscouttrafpol_game.dat'
+
+data_file 'AUDIO_SOUNDDATA' 'audio/jmsahp_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/dlc_delperropd_sounds.dat'
+
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/delperropd'
